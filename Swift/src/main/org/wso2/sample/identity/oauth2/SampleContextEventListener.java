37f9ca177fc6df8ab4838fd8aa627185e77fc316
nilasini <nilathiru93@gmail.com>
2018-01-07 20:47:21 +0530

    Modify the code to update the license and pom

--- Swift/src/main/org/wso2/sample/identity/oauth2/SampleContextEventListener.java
+++ Swift/src/main/org/wso2/sample/identity/oauth2/SampleContextEventListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -35,7 +35,7 @@ public class SampleContextEventListener implements ServletContextListener {
         properties = new Properties();
         try {
             properties.load(servletContextEvent.getServletContext().
-                    getResourceAsStream("/WEB-INF/classes/dispatch.properties"));
+                    getResourceAsStream("/WEB-INF/classes/swift.properties"));
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
         }
